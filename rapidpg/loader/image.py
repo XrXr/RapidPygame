649fa3ad4798d09b6bda71923860435387fa2f0c
XrXr <xrsquared@gmail.com>
2014-05-31 15:38:06 -0400

    Image loader and example

---
+++ rapidpg/loader/image.py
@@ -0,0 +1,64 @@
+"""
+ Rapid Pygame
+ https://github.com/XrXr/RapidPygame
+ License: MIT
+"""
+import os
+from os.path import join, isfile, splitext
+from pygame.image import load
+
+
+class ImageLoader:
+    def __init__(self, origin):
+        """
+        Create a loader pointing at the *origin*. Any loading is relative to the *origin*
+        Paths passed to the methods of this object are lists where each element is either
+        a directory name or a file name. ([dir1,dir2,file.txt])
+        :param origin: path string
+        """
+        self.origin = origin
+
+    def load_image(self, path, convert_alpha=False):
+        """
+        Load an image file
+        :param path: a path to the image in list format
+        :param convert_alpha: *boolean* convert surfaces with convert_alpha()
+        :return: surface
+        """
+        if convert_alpha:
+            return load(self.get_path(path)).convert_alpha()
+        return load(self.get_path(path)).convert()
+
+    def load_frames(self, path, frames, convert_alpha=False):
+        """
+        Load a sequence of image named 1.b...*frames*.b from *path*
+        :param path: a path in list format pointing to a directory
+        :param frames: number of frames to load
+        :param convert_alpha: *boolean* convert surfaces with convert_alpha()
+        :return: a list of surfaces
+        """
+        dir_path = self.get_path(path)
+        extension = ""
+        for name in os.listdir(dir_path):
+            file = join(dir_path, name)
+            if isfile(file):
+                striped, file_ext = splitext(name)
+                if striped == "1":
+                    extension = file_ext
+                    break
+        surfs = []
+        for c in range(1, frames + 1):
+            file = join(dir_path, str(c) + extension)
+            if convert_alpha:
+                surfs.append(load(file).convert_alpha())
+                continue
+            surfs.append(load(file).convert())
+        return surfs
+
+    def get_path(self, path):
+        """
+        Returns a path string relative to the origin
+        :param path: a path in list format
+        :return: path string
+        """
+        return join(self.origin, *path)
