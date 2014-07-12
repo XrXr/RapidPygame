3e1dbdac884cb90c5a6f745c66d515b74ba0db0c
XrXr <xrsquared@gmail.com>
2014-07-12 12:27:58 -0400

    Add packaging info for pypi

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+  name = 'rapidpg',
+  packages = ['rapidpg'],
+  version = '0.3.1',
+  description = 'A collections of tools for rapid game developement with pygame',
+  author = 'XrXr',
+  url = 'https://github.com/XrXr/RapidPygame',
+  download_url = 'https://github.com/XrXr/RapidPygame/tarball/0.3.1'
+  keywords = ['game developement', 'pygame'],
+)
