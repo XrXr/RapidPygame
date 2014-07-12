4fb99ae219ca6b8e8c41199fd1436aa6b4829c1f
XrXr <xrsquared@gmail.com>
2014-07-12 13:01:04 -0400

    Correct setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
   name = 'rapidpg',
-  packages = ['rapidpg'],
+  packages = find_packages(),
   version = '0.3.1',
   description = 'A collections of tools for rapid game developement with pygame',
   author = 'XrXr',
+  author_email = 'xrsquared@gmail.com',
   url = 'https://github.com/XrXr/RapidPygame',
-  download_url = 'https://github.com/XrXr/RapidPygame/tarball/0.3.1'
+  download_url = 'https://github.com/XrXr/RapidPygame/tarball/0.3.1',
   keywords = ['game developement', 'pygame'],
 )
